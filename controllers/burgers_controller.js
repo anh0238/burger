67a76ec819b2f49e609898f8bb49f3afdf9ad566
ahend <anh0238@gmail.com>
2018-10-17 15:31:04 -0500

    updates

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,11 +1,41 @@
 var express = require("express");
 var router = express.Router();
 
-var burger = require("../models");
+var burger = require("../models/burger.js");
 
-//routes
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burger: data
+        };
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("api/burgers/:id", function(req, res) {
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
 
 
+router.put("api/burgers", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 
 module.exports = router;
